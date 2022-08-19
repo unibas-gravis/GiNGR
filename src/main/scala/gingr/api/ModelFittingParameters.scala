8c82e409b634ebedca6bcd8428e2e50aac6c4a70
dennis.madsen <dennis.madsen@unibas.ch>
2022-08-19 10:43:29 +0200

    Added save/load possibility of modelFittingParameters in json format

--- src/main/scala/gingr/api/ModelFittingParameters.scala
+++ src/main/scala/gingr/api/ModelFittingParameters.scala
@@ -22,6 +22,11 @@ import scalismo.geometry.{EuclideanVector, Point, _3D}
 import scalismo.mesh.TriangleMesh
 import scalismo.statisticalmodel.PointDistributionModel
 import scalismo.transformations._
+import spray.json.DefaultJsonProtocol._
+import spray.json.{RootJsonFormat, _}
+
+import java.io.{BufferedWriter, File, FileWriter, IOException}
+import scala.io.Source
 
 case class ScaleParameter(s: Double) {
   def parameters: DenseVector[Double] = DenseVector(s)
@@ -32,7 +37,7 @@ case class EulerAngles(phi: Double, theta: Double, psi: Double) {
 }
 
 case class EulerRotation(angles: EulerAngles, center: Point[_3D]) {
-  val rotation: Rotation[_3D]         = Rotation(angles.phi, angles.theta, angles.psi, center)
+  def rotation: Rotation[_3D]         = Rotation(angles.phi, angles.theta, angles.psi, center)
   def parameters: DenseVector[Double] = DenseVector.vertcat(angles.parameters, center.toBreezeVector)
 }
 
@@ -50,7 +55,7 @@ case class PoseParameters(translation: EuclideanVector[_3D], rotation: EulerRota
 case class ShapeParameters(parameters: DenseVector[Double])
 
 case class ModelFittingParameters(scale: ScaleParameter, pose: PoseParameters, shape: ShapeParameters) {
-  val allParameters: DenseVector[Double] = DenseVector.vertcat(scale.parameters, pose.parameters, shape.parameters)
+  def allParameters: DenseVector[Double] = DenseVector.vertcat(scale.parameters, pose.parameters, shape.parameters)
 
   def rigidTransform: TranslationAfterRotation[_3D] = {
     val translation = Translation(pose.translation)
@@ -67,8 +72,40 @@ case class ModelFittingParameters(scale: ScaleParameter, pose: PoseParameters, s
 
   def scaleTransform: Scaling[_3D] = Scaling3D(scale.s)
 }
+object ModelFittingParametersJson {
+  implicit object RootJsDenseVectorFormat extends RootJsonFormat[DenseVector[Double]] {
+    def write(value: DenseVector[Double]): JsValue = value.toArray.toJson
+    def read(value: JsValue): DenseVector[Double] = value match {
+      case x: JsArray => DenseVector[Double](x.convertTo[Array[Double]])
+      case _          => deserializationError("JSON array expected")
+    }
+  }
+  implicit object RootJsPoint3DFormat extends RootJsonFormat[Point[_3D]] {
+    def write(value: Point[_3D]): JsValue = value.toArray.toJson
+    def read(value: JsValue): Point[_3D] = value match {
+      case x: JsArray => Point[_3D](x.convertTo[Array[Double]])
+      case _          => deserializationError("JSON array expected")
+    }
+  }
+
+  implicit object RootJsEuclideanVector3DFormat extends RootJsonFormat[EuclideanVector[_3D]] {
+    def write(value: EuclideanVector[_3D]): JsValue = value.toArray.toJson
+    def read(value: JsValue): EuclideanVector[_3D] = value match {
+      case x: JsArray => EuclideanVector[_3D](x.convertTo[Array[Double]])
+      case _          => deserializationError("JSON array expected")
+    }
+  }
+
+  implicit val myJsonShapePars: RootJsonFormat[ShapeParameters]        = jsonFormat1(ShapeParameters.apply)
+  implicit val myJsonAngPars: RootJsonFormat[EulerAngles]              = jsonFormat3(EulerAngles.apply)
+  implicit val myJsonRotPars: RootJsonFormat[EulerRotation]            = jsonFormat2(EulerRotation.apply)
+  implicit val myJsonPosePars: RootJsonFormat[PoseParameters]          = jsonFormat2(PoseParameters.apply)
+  implicit val myJsonScalePars: RootJsonFormat[ScaleParameter]         = jsonFormat1(ScaleParameter.apply)
+  implicit val myJsonModelPars: RootJsonFormat[ModelFittingParameters] = jsonFormat3(ModelFittingParameters.apply)
+}
 
 object ModelFittingParameters {
+  import ModelFittingParametersJson._
   def apply(modelRank: Int): ModelFittingParameters = {
     val pose = PoseParameters(
       translation = EuclideanVector(0, 0, 0),
@@ -103,5 +140,23 @@ object ModelFittingParameters {
   ): TriangleMesh[_3D] = {
     val instance = modelInstanceShapePose(model, pars)
     instance.transform(pars.scaleTransform)
+  }
+
+  def save(pars: ModelFittingParameters, file: File): Unit = {
+    try {
+      val writer = new BufferedWriter(new FileWriter(file))
+      writer.write(pars.toJson.prettyPrint)
+      writer.close()
+    } catch {
+      case e: Exception => throw new IOException("Writing JSON log file failed!")
+    }
+  }
+
+  def load(file: File): ModelFittingParameters = {
+    println(s"Loading JSON file: ${file.toString}")
+    val src  = Source.fromFile(file.toString)
+    val data = src.mkString.parseJson.convertTo[ModelFittingParameters]
+    src.close()
+    data
   }
 }
