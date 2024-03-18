acc3c8e592e3ebdace5405608558a9688851d815
Dennis Madsen <madsendennis@gmail.com>
2024-03-18 10:15:25 +0100

    Release 1.0 (#11)
    * misc: update scalismo version

--- examples/project.scala
+++ examples/project.scala
@@ -1,3 +1,3 @@
 //> using scala "3.3"
-//> using dep "ch.unibas.cs.gravis::gingr:1.0-RC1"
 //> using dep "ch.unibas.cs.gravis::scalismo-ui:1.0-RC1"
+//> using dep "ch.unibas.cs.gravis::gingr:1.0-RC1"
